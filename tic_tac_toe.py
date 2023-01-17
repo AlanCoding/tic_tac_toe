2ef96eb8c86a54fdc48109b512132e45c9ce57b7
Alan Rominger <arominge@redhat.com>
2023-01-16 21:51:21 -0500

    Add code from my first ChatGPT prompt

---
+++ tic_tac_toe.py
@@ -0,0 +1,43 @@
+import pygame
+
+# initialize pygame
+pygame.init()
+
+# set up the display
+size = (400, 400)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Tic-Tac-Toe")
+
+# define colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+# create a 2D array to represent the game board
+board = [[0 for x in range(3)] for y in range(3)]
+
+# define the game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # clear the screen
+    screen.fill(white)
+
+    # draw the game board
+    for y in range(3):
+        for x in range(3):
+            if board[y][x] == 0:
+                pygame.draw.rect(screen, black, (x*133, y*133, 133, 133), 2)
+            elif board[y][x] == 1:
+                pygame.draw.line(screen, black, (x*133, y*133), ((x+1)*133, (y+1)*133), 2)
+                pygame.draw.line(screen, black, ((x+1)*133, y*133), (x*133, (y+1)*133), 2)
+            else:
+                pygame.draw.circle(screen, black, (int((x+0.5)*133), int((y+0.5)*133)), int(133/2), 2)
+
+    # update the display
+    pygame.display.flip()
+
+# close the game
+pygame.quit()
